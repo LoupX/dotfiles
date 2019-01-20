f44aa2b1caf99b70f4440722cb8226d4a2961c43
Diego Romero <jd@inkatze.com>
2019-01-20 08:32:26 -0800

    Added Go environment and configuration to dotfiles (#19)

--- files/config.fish
+++ files/config.fish
@@ -6,16 +6,21 @@ alias c 'z'
 if status --is-login
     set -xg SPACEFISH_CHAR_SUFFIX '  '
 
+    set -l GOPATH $HOME/dev/go
+    set -l GOBIN $GOPATH/bin
+    mkdir -p $GOPATH
+    set -xg GOPATH $GOPATH
+    set -xg GOBIN $GOBIN
+
     set -xg PYENV_ROOT $HOME/.pyenv
     set -xg PIPENV_DEFAULT_PYTHON_VERSION 3.6
     set -xg DEVPI_DIA "https://devpi.dia.com/datateam/releases/+simple"
-    set -xg PIP_EXTRA_INDEX_URL $DEVPI_DIA https://pypi.python.org/simple
-
     set -l PYTHON_LIB_EXEC /usr/local/opt/python/libexec/bin
+    set -l PYENV_DIR ~/.pyenv
+
     set -l POSTGRES_BIN /Applications/Postgres.app/Contents/Versions/latest/bin
     set -l USER_BINS /usr/local/bin
-    set -l PYENV_DIR ~/.pyenv
-    set -xg PATH $USER_BINS $PYENV_DIR $POSTGRES_BIN $PYTHON_LIB_EXEC $PATH
+    set -xg PATH $USER_BINS $GOBIN $PYENV_DIR $POSTGRES_BIN $PYTHON_LIB_EXEC $PATH
 end
 
 set -xg GPG_TTY (tty)
